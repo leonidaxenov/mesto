04f96f19159ce2c38926f18d4611698a48361776
Axenov <leonid@mgc.mentorg.com>
2022-05-27 11:49:22 +0300

    add element

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,72 @@
+function submitEdit(event)
+{
+  event.preventDefault();
+  const name=document.querySelector('.profile__name');
+  const input_name=document.querySelector('.popup__input_name');
+  name.textContent=input_name.value;
+  const desc=document.querySelector('.profile__description');
+  const input_desc=document.querySelector('.popup__input_description');
+  desc.textContent=input_desc.value;
+  closePopup(event);
+}
+
+function submitAdd(event)
+{
+  event.preventDefault();
+  const input_name=document.querySelector('.popup__input_name');
+  const input_desc=document.querySelector('.popup__input_description');
+  addCard(input_name.value,input_desc.value);
+  closePopup(event);
+}
+
+function showPopup(event)
+{
+  const header=document.querySelector('.popup__header');
+  const input_name=document.querySelector('.popup__input_name');
+  const input_desc=document.querySelector('.popup__input_description');
+  const replaceFontColor=(element, is_edit)=>{
+    if(is_edit){
+      element.classList.remove('popup__input_font_color-add');
+      element.classList.add('popup__input_font_color-edit');
+    }
+    else {
+      element.classList.add('popup__input_font_color-add');
+      element.classList.remove('popup__input_font_color-edit');
+    }
+  };
+  const replaceFontColorOnEdit=(event)=>replaceFontColor(event.currentTarget,true);
+  const form=document.querySelector('.popup__container');
+  if(event.currentTarget.classList.contains('profile__edit-button')){
+    header.textContent = "Редактировать профиль";
+    replaceFontColor(input_name,true);
+    replaceFontColor(input_desc,true);
+    const name=document.querySelector('.profile__name');
+    const desc=document.querySelector('.profile__description');
+    input_name.value=name.textContent;
+    input_desc.type='text';
+    input_desc.value=desc.textContent;
+    input_name.removeEventListener('input',replaceFontColorOnEdit);
+    input_desc.removeEventListener('input',replaceFontColorOnEdit);
+    form.addEventListener('submit', submitEdit);
+    form.removeEventListener('submit', submitAdd);
+  }
+  else if(event.currentTarget.classList.contains('profile__add-button')){
+    header.textContent = "Новое место";
+    replaceFontColor(input_name,false);
+    replaceFontColor(input_desc,false);
+    input_name.value="Название";
+    input_name.addEventListener('input',replaceFontColorOnEdit);
+    input_desc.type='url';
+    input_desc.value="Ссылка на картинку";
+    input_desc.addEventListener('input',replaceFontColorOnEdit);
+    form.removeEventListener('submit', submitEdit);
+    form.addEventListener('submit', submitAdd);
+ }
+  let popup=document.querySelector('.popup');
+  popup.classList.remove('popup_hidden');
+  popup.classList.add('popup_visible');
+}
+
 function closePopup(event)
 {
   let popup=document.querySelector('.popup');
@@ -10,33 +79,14 @@ window.addEventListener('scroll',(event)=> {
   popup.style.top=`${window.pageYOffset}px`;
 });
 
-let editButton=document.querySelector('.profile__edit-button');
-editButton.addEventListener('click', (event)=> {
-  let name=document.querySelector('.profile__name');
-  let input_name=document.querySelector('.popup__input_name');
-  input_name.value=name.textContent;
-  let desc=document.querySelector('.profile__description');
-  let input_desc=document.querySelector('.popup__input_description');
-  input_desc.value=desc.textContent;
-  let popup=document.querySelector('.popup');
-  popup.classList.remove('popup_hidden');
-  popup.classList.add('popup_visible');
-});
+const editButton=document.querySelector('.profile__edit-button');
+editButton.addEventListener('click', showPopup);
 
-let closeButton=document.querySelector('.popup__close-button');
+const addButton=document.querySelector('.profile__add-button');
+addButton.addEventListener('click', showPopup);
+
+const closeButton=document.querySelector('.popup__close-button');
 closeButton.addEventListener('click', closePopup);
-
-let form=document.querySelector('.popup__container');
-form.addEventListener('submit', (event)=> {
-  event.preventDefault();
-  let name=document.querySelector('.profile__name');
-  let input_name=document.querySelector('.popup__input_name');
-  name.textContent=input_name.value;
-  let desc=document.querySelector('.profile__description');
-  let input_desc=document.querySelector('.popup__input_description');
-  desc.textContent=input_desc.value;
-  closePopup(event);
-});
 
 function addCard(name,link){
   const userTemplate = document.querySelector('#card').content;
